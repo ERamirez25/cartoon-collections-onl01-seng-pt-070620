6805868dd7941c14482cc4028a512bd66cc7cd01
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-15 01:24:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,14 @@ end
 
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  i = 0
+  while i < array.length
+   new_array << array[i].capitalize + !
+   i += 1
+ end 
+  
 end
 
 def long_planeteer_calls# code an argument here
