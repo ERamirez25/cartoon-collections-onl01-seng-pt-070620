28d980fde5f0674c444f88d53cf623b57b1d884b
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-15 01:34:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find do |cheddar|
+  array.find do |cheese|
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 end 
